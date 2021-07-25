a1af3b2ea08da36d5d5dab9348439ce99e46362f
Arslek <62049765+AlfViklund@users.noreply.github.com>
2021-07-25 20:11:38 +0500

    2.1.4

--- src/main/java/koschei/models/Needle7.java
+++ src/main/java/koschei/models/Needle7.java
@@ -1,7 +1,10 @@
 package koschei.models;
 
-public class Needle7 {
+import org.springframework.beans.factory.annotation.Autowired;
 
+public class Needle7 {
+    @Autowired
+    private Deth8 deth8;
     @Override
     public String toString() {
         return ", смерть Кощея на игле :( " + "";
